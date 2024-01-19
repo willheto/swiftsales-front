c928acfce45449437551c5d3d05cde381030e4a9
unknown <henritopias.willman@gmail.com>
2024-01-19 17:32:15 +0200

    added dev api url

--- globals.ts
+++ globals.ts
@@ -2,6 +2,8 @@ const setupAPI = () => {
 	switch (process.env.NODE_ENV) {
 		case 'local':
 			return 'http://192.168.33.10';
+		case 'development':
+			return 'https://dev-api.swiftsales.fi';
 		default:
 			return 'http://192.168.33.10';
 	}
